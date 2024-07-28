37b1d77e8cdad320d2dc22f85ed5cd00f1c739d1
Andre Sitorus <andre.yehuda@sirclo.com>
2024-07-28 21:23:51 +0700

    feat: add new output from s3-bucket module

--- terraform/modules/s3-bucket/outputs.tf
+++ terraform/modules/s3-bucket/outputs.tf
@@ -1,6 +1,11 @@
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket"
+output "s3_bucket_id" {
+  description = "The ID of the S3 bucket"
   value       = aws_s3_bucket.this.id
+}
+
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.this.arn
 }
 
 output "iam_group_name" {
